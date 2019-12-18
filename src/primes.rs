27c734f6143eb439a0cfaec603e15fb9c24ca05f
Yuchen Lin <yuchennil@gmail.com>
2019-12-17 23:57:43 -0800

    Move factoring from Sieve; implement Pollard's rho

--- src/primes.rs
+++ src/primes.rs
@@ -110,25 +110,37 @@ impl Sieve {
         totient
     }
 
-    fn prime_factor(&self, n: u64) -> collections::BTreeMap<u64, u32> {
-        assert_ne!(0, n, "prime factors of n undefined for n == 0");
-        assert!(n <= self.limit, "Sieve must contain all primes at or below n");
-        let mut n = n;
-        let mut prime_factors = collections::BTreeMap::new();
-        for &p in self.iter() {
-            while n % p == 0 {
-                *prime_factors.entry(p).or_insert(0) += 1;
-                n /= p;
-            }
-            if n == 1 {
-                break;
-            }
-        }
-        prime_factors
+    pub fn iter(&self) -> slice::Iter<u64> {
+        self.primes.iter()
     }
 
-    pub fn factor(&self, n: u64) -> Vec<u64> {
-        let prime_factors = self.prime_factor(n).into_iter().collect::<Vec<_>>();
+    pub fn into_iter(self) -> std::vec::IntoIter<u64> {
+        self.primes.into_iter()
+    }
+}
+
+pub struct Factory {
+    sieve: Sieve,
+}
+
+impl Factory {
+
+    pub fn new(limit: u64) -> Factory {
+        Factory {
+            sieve: Sieve::eratosthenes(limit),
+        }
+    }
+
+    pub fn prime_factor(&self, n: u64) -> Vec<(u64, u32)> {
+        let mut prime_factors = collections::BTreeMap::new();
+        for p in self.trial_division(n) {
+            *prime_factors.entry(p).or_insert(0) += 1;
+        }
+        prime_factors.into_iter().collect()
+    }
+
+    pub fn all_factors(&self, n: u64) -> Vec<u64> {
+        let prime_factors = self.prime_factor(n);
         let mut factors = Vec::new();
         for factor_combination in combinatorics::CombinationsWithRepeats::new(&prime_factors) {
             factors.push(factor_combination.into_iter().map(
@@ -138,12 +150,55 @@ impl Sieve {
         factors
     }
 
-    pub fn iter(&self) -> slice::Iter<u64> {
-        self.primes.iter()
+    /// Repeatedly divide by primes smaller than n until n == 1.
+    fn trial_division(&self, n: u64) -> Vec<u64> {
+        assert_ne!(0, n, "prime factors of n undefined for n == 0");
+        assert!(n <= self.sieve.limit, "Sieve must contain all primes at or below n");
+        let mut n = n;
+        let mut prime_factors = Vec::new();
+        for &p in self.sieve.iter() {
+            while n % p == 0 {
+                prime_factors.push(p);
+                n /= p;
+            }
+            if n == 1 {
+                break;
+            }
+        }
+        prime_factors
     }
 
-    pub fn into_iter(self) -> std::vec::IntoIter<u64> {
-        self.primes.into_iter()
+    /// Pollard's rho Monte Carlo factor-finding with Floyd's cycle detection
+    /// Return a nontrivial factor, or failure.
+    fn pollards_rho(n: u64) -> Option<u64> {
+        fn f(n: u64) -> u64 {
+            n * n + 1
+        }
+        fn difference(a: u64, b: u64) -> u64 {
+            if a > b {
+                a - b
+            } else {
+                b - a
+            }
+        }
+        if n < 2 {
+            return None
+        }
+        if n == 2 {
+            return Some(2)
+        }
+        let mut x = 2;
+        let mut y = 2;
+        let mut d = 1;
+        while d == 1 {
+            x = f(x) % n;
+            y = f(f(y)) % n;
+            d = gcd(difference(x, y), n);
+        }
+        match d == n {
+            true => None,
+            false => Some(d),
+        }
     }
 }
 
@@ -229,35 +284,45 @@ mod tests {
 
     #[test]
     fn prime_factor_correct() {
-        let sieve = Sieve::eratosthenes(12);
-        assert_eq!(vec![(&0, &0); 0], sieve.prime_factor(1).iter().collect::<Vec<_>>());
-        assert_eq!(vec![(&2, &1)], sieve.prime_factor(2).iter().collect::<Vec<_>>());
-        assert_eq!(vec![(&3, &1)], sieve.prime_factor(3).iter().collect::<Vec<_>>());
-        assert_eq!(vec![(&2, &2)], sieve.prime_factor(4).iter().collect::<Vec<_>>());
-        assert_eq!(vec![(&2, &2), (&3, &1)], sieve.prime_factor(12).iter().collect::<Vec<_>>());
+        let factory = Factory::new(12);
+        assert_eq!(vec![(0, 0); 0], factory.prime_factor(1));
+        assert_eq!(vec![(2, 1)], factory.prime_factor(2));
+        assert_eq!(vec![(3, 1)], factory.prime_factor(3));
+        assert_eq!(vec![(2, 2)], factory.prime_factor(4));
+        assert_eq!(vec![(2, 2), (3, 1)], factory.prime_factor(12));
     }
 
     #[test]
     #[should_panic]
     fn prime_factor_zero() {
-        let sieve = Sieve::eratosthenes(7);
-        sieve.prime_factor(0);
+        let factory = Factory::new(7);
+        factory.prime_factor(0);
     }
 
     #[test]
     #[should_panic]
     fn prime_factor_sieve_too_small() {
-        let sieve = Sieve::eratosthenes(7);
-        sieve.prime_factor(50);
+        let factory = Factory::new(7);
+        factory.prime_factor(50);
     }
 
     #[test]
-    fn factor_correct() {
-        let sieve = Sieve::eratosthenes(12);
-        assert_eq!(vec![&1], sieve.factor(1).iter().collect::<Vec<_>>());
-        assert_eq!(vec![&1, &2], sieve.factor(2).iter().collect::<Vec<_>>());
-        assert_eq!(vec![&1, &3], sieve.factor(3).iter().collect::<Vec<_>>());
-        assert_eq!(vec![&1, &2, &4], sieve.factor(4).iter().collect::<Vec<_>>());
-        assert_eq!(vec![&1, &2, &3, &4, &6, &12], sieve.factor(12).iter().collect::<Vec<_>>());
+    fn all_factors_correct() {
+        let factory = Factory::new(12);
+        assert_eq!(vec![1], factory.all_factors(1));
+        assert_eq!(vec![1, 2], factory.all_factors(2));
+        assert_eq!(vec![1, 3], factory.all_factors(3));
+        assert_eq!(vec![1, 2, 4], factory.all_factors(4));
+        assert_eq!(vec![1, 2, 3, 4, 6, 12], factory.all_factors(12));
+    }
+
+    #[test]
+    fn pollards_rho_correct() {
+        assert_eq!(None, Factory::pollards_rho(0));
+        assert_eq!(None, Factory::pollards_rho(1));
+        assert_eq!(Some(2), Factory::pollards_rho(2));
+        assert_eq!(Some(3), Factory::pollards_rho(2 * 3));
+        assert_eq!(Some(5), Factory::pollards_rho(5 * 17));
+        assert_eq!(Some(271), Factory::pollards_rho(271 * 241));
     }
 }
