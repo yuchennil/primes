b1b87f15237022b28544e47192f5c4a74978613b
Yuchen Lin <yuchennil@gmail.com>
2020-01-30 22:00:15 -0800

    Refactor wheel sieve logic to a new struct

--- src/primes.rs
+++ src/primes.rs
@@ -30,132 +30,20 @@ impl Sieve {
 
     /// Run a segmented Sieve of Eratosthenes to save memory while generating primes.
     ///
-    /// The segment length is just above sqrt(limit) so that the origin segment suffices to sieve
+    /// segment_length is just above sqrt(limit) so that the origin segment suffices to sieve
     /// all remaining segments (which hence don't need to be kept in memory after we've finished
     /// sieving through them).
     pub fn segmented(limit: u64) -> Sieve {
         let segment_length = cmp::max(1, (limit as f64).sqrt().ceil() as u64);
-        let origin = SieveSegment::eratosthenes(segment_length);
 
-        let mut segments = Vec::new();
+        let mut segments = vec![SieveSegment::eratosthenes(segment_length)];
         for start in (segment_length..limit).step_by(segment_length as usize) {
             let end = cmp::min(start + segment_length, limit);
-            segments.push(SieveSegment::from_origin(start, end, &origin));
+            segments.push(SieveSegment::from_origin(start, end, &segments[0]));
         }
 
-        Sieve {
-            segments: vec![origin].into_iter().chain(segments).collect(),
-            limit,
-        }
+        Sieve { segments, limit }
     }
-}
-
-#[derive(Debug)]
-struct SieveSegment {
-    primes: Vec<u64>,
-    start: u64,
-    end: u64,
-}
-
-impl SieveSegment {
-    /// Sieve an origin segment [0, limit) using Eratosthenes optimized by skipping evens after 2.
-    fn eratosthenes(limit: u64) -> SieveSegment {
-        if limit <= 2 {
-            return SieveSegment {
-                primes: Vec::new(),
-                start: 0,
-                end: limit,
-            };
-        }
-
-        let mut sieve = vec![true; to_sieve(limit + 1)];
-        // to_sieve(1) == 0
-        sieve[0] = false;
-
-        let mut p = 3;
-        while p * p <= limit {
-            // Optimize by starting the multiples search at p^2, p^2 + 2p, ...
-            // instead of 2p, 3p, ...
-            //
-            // Note that a step size of p in the sieve corresponds to a step of 2 * p in u64s.
-            for multiple in (to_sieve(p * p)..sieve.len()).step_by(p as usize) {
-                sieve[multiple] = false;
-            }
-            match (to_sieve(p) + 1..sieve.len()).find(|&n| sieve[n]) {
-                Some(next) => p = to_prime(next),
-                None => break,
-            }
-        }
-
-        SieveSegment {
-            primes: vec![2]
-                .into_iter()
-                .chain(sieve.iter().enumerate().filter_map(|(p, &x)| {
-                    if x {
-                        Some(to_prime(p))
-                    } else {
-                        None
-                    }
-                }))
-                .collect(),
-            start: 0,
-            end: limit,
-        }
-    }
-
-    /// Sieve a segment [start, end) based on an origin segment.
-    fn from_origin(start: u64, end: u64, origin: &SieveSegment) -> SieveSegment {
-        assert_eq!(0, origin.start);
-        assert!(origin.end.pow(2) >= end);
-
-        // If start is even, then set sieve_start to point at the first odd after it: start + 1.
-        // If start is odd, then adding 1 won't make a difference: to_sieve(start + 1) will still
-        // point at start. Same with sieve_end.
-        let sieve_start = to_sieve(start + 1);
-        let sieve_end = to_sieve(end + 1);
-        let sieve_size = sieve_end - sieve_start;
-        let mut sieve = vec![true; sieve_size];
-        // Don't sieve 2 if it exists.
-        for &p in origin.primes.iter().skip(1) {
-            // Optimize by starting the multiples search at p^2, or the first odd multiple of p
-            // after start, whichever is greater.
-            let p_sieve_start =
-                to_sieve(cmp::max(p * p, p * ceil_odd(ceil_div(start, p)))) - sieve_start;
-            for multiple in (p_sieve_start..sieve_size).step_by(p as usize) {
-                sieve[multiple] = false;
-            }
-        }
-
-        let mut primes = Vec::new();
-        // We've only sieved odd primes, so 2 will need to be manually prepended, if necessary.
-        if start <= 2 && end > 2 {
-            primes.push(2);
-        }
-        primes.extend(sieve.into_iter().enumerate().filter_map(|(p, x)| {
-            if x {
-                Some(to_prime(p + sieve_start))
-            } else {
-                None
-            }
-        }));
-        SieveSegment { primes, start, end }
-    }
-}
-
-/// Convenience functions to convert between prime space (u64 numbers) and sieve space
-/// (usizes corresponding to encoded odd u64s).
-fn to_sieve(prime: u64) -> usize {
-    ((prime - 1) / 2) as usize
-}
-fn to_prime(sieve: usize) -> u64 {
-    2 * sieve as u64 + 1
-}
-/// Convenience functions to find u64 ceilings
-fn ceil_div(numerator: u64, divisor: u64) -> u64 {
-    numerator / divisor + ((numerator % divisor) != 0) as u64
-}
-fn ceil_odd(n: u64) -> u64 {
-    n + (n + 1) % 2
 }
 
 impl IntoIterator for Sieve {
@@ -170,6 +58,144 @@ impl IntoIterator for Sieve {
             .flatten()
             .collect::<Vec<_>>();
         result.into_iter()
+    }
+}
+
+#[derive(Debug)]
+struct SieveSegment {
+    primes: Vec<u64>,
+    start: u64,
+    end: u64,
+}
+
+impl SieveSegment {
+    /// Sieve an origin segment [0, limit) using Eratosthenes optimized by skipping evens after 2.
+    fn eratosthenes(limit: u64) -> SieveSegment {
+        let mut sieve = WheelSieveSegment::create(0, limit);
+
+        let mut p = 3;
+        while p * p <= limit {
+            sieve.strike_prime(p);
+            if let Some(next_p) = sieve.next(p) {
+                p = next_p;
+            } else {
+                break;
+            }
+        }
+
+        SieveSegment {
+            primes: sieve.unpack_primes(),
+            start: 0,
+            end: limit,
+        }
+    }
+
+    /// Sieve a segment [start, end) based on an origin segment.
+    fn from_origin(start: u64, end: u64, origin: &SieveSegment) -> SieveSegment {
+        assert_eq!(0, origin.start);
+        assert!(origin.end.pow(2) >= end);
+
+        let mut sieve = WheelSieveSegment::create(start, end);
+        // Don't sieve 2 if it exists.
+        for &p in origin.primes.iter().skip(1) {
+            sieve.strike_prime(p);
+        }
+
+        SieveSegment {
+            primes: sieve.unpack_primes(),
+            start,
+            end,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct WheelSieveSegment {
+    data: Vec<bool>,
+    offset: usize,
+    start: u64,
+    end: u64,
+}
+
+impl WheelSieveSegment {
+    // Create an unsieved WheelSieveSegment in [start, end).
+    fn create(start: u64, end: u64) -> WheelSieveSegment {
+        // If start is even, then set sieve_offset to point at the first odd after it: start + 1.
+        // If start is odd, then adding 1 won't make a difference: u64_to_sieve(start + 1) will
+        // still point at start. Likewise with end.
+        let offset = WheelSieveSegment::u64_to_sieve(start + 1);
+        let size = WheelSieveSegment::u64_to_sieve(end + 1) - offset;
+        let mut data = vec![true; size];
+        if offset == 0 {
+            // Strike 1 from the sieve if it's within bounds, as sieving wouldn't remove it.
+            if let Some(e) = data.get_mut(0) {
+                *e = false;
+            }
+        }
+        WheelSieveSegment {
+            data,
+            offset,
+            start,
+            end,
+        }
+    }
+
+    // Strike multiples of prime in sieve.
+    //
+    // Optimize by starting the multiples search at the greater of
+    // - p^2 (smaller multiples should already have been struck by previous primes)
+    // - the first odd multiple of p after start
+    //
+    // Note that a step size of p in the sieve corresponds to a step of 2 * p in u64s.
+    fn strike_prime(&mut self, p: u64) {
+        /// Convenience functions to find u64 ceilings
+        fn ceil_div(numerator: u64, divisor: u64) -> u64 {
+            numerator / divisor + ((numerator % divisor) != 0) as u64
+        }
+        fn ceil_odd(n: u64) -> u64 {
+            n + (n + 1) % 2
+        }
+
+        let p_sieve_start =
+            WheelSieveSegment::u64_to_sieve(cmp::max(p * p, p * ceil_odd(ceil_div(self.start, p))))
+                - self.offset;
+        for multiple in (p_sieve_start..self.data.len()).step_by(p as usize) {
+            self.data[multiple] = false;
+        }
+    }
+
+    /// Find the next prime after p in the sieve, or None
+    fn next(&self, p: u64) -> Option<u64> {
+        Some(WheelSieveSegment::sieve_to_u64(
+            (WheelSieveSegment::u64_to_sieve(p) + 1..self.data.len()).find(|&n| self.data[n])?,
+        ))
+    }
+
+    /// Consume this WheelSieveSegment to unpack primes
+    fn unpack_primes(self) -> Vec<u64> {
+        let mut primes = Vec::new();
+        // We've only sieved odd primes, so 2 will need to be manually prepended, if necessary.
+        if self.start <= 2 && self.end > 2 {
+            primes.push(2);
+        }
+        let offset = self.offset;
+        primes.extend(self.data.into_iter().enumerate().filter_map(|(p, x)| {
+            if x {
+                Some(WheelSieveSegment::sieve_to_u64(p + offset))
+            } else {
+                None
+            }
+        }));
+        primes
+    }
+
+    /// Convenience functions to convert between prime space (u64 numbers) and sieve space
+    /// (usizes corresponding to encoded odd u64s).
+    fn u64_to_sieve(prime: u64) -> usize {
+        ((prime - 1) / 2) as usize
+    }
+    fn sieve_to_u64(sieve: usize) -> u64 {
+        2 * sieve as u64 + 1
     }
 }
 
@@ -253,6 +279,14 @@ mod tests {
     fn sieve_segment_from_origin() {
         let origin = SieveSegment::eratosthenes(50);
 
+        let sieve_segment = SieveSegment::from_origin(0, 50, &origin);
+        assert_eq!(0, sieve_segment.start);
+        assert_eq!(50, sieve_segment.end);
+        assert_eq!(
+            vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47],
+            sieve_segment.primes.to_vec()
+        );
+
         let sieve_segment = SieveSegment::from_origin(50, 100, &origin);
         assert_eq!(50, sieve_segment.start);
         assert_eq!(100, sieve_segment.end);
@@ -292,10 +326,5 @@ mod tests {
             vec![53, 59, 61, 67, 71, 73, 79, 83, 89, 97],
             sieve_segment.primes.to_vec()
         );
-    }
-
-    #[test]
-    fn segmented_bench() {
-        let sieve = Sieve::segmented(10_u64.pow(9));
     }
 }
