c7c684905c76ac98e07e8f9af49b3079e5f4e610
Yuchen Lin <yuchennil@gmail.com>
2020-03-17 05:31:15 -0700

    Document Segment algorithm design and tradeoffs

--- src/primes.rs
+++ src/primes.rs
@@ -431,6 +431,33 @@ impl WheelSegment {
     }
 }
 
+/// A Segment of the sieve consists of several spokes within the range [segment_start, segment_end).
+/// Each spoke is a residue class of the multiplicative group of integers modulo n: (Z / n Z)^x,
+/// where n is a primorial. For instance, for n = 2 * 3 and range [12, 30) we have:
+///     # 1 mod 6   # 5 mod 6
+///     spokes[0]   spokes[1]
+///    [13,        [17,
+///     19,         23,
+///     25]         29]
+///
+/// Sieving only within (Z / n Z)^x lets us skip a guaranteed SPOKE_SIZE / WHEEL_SIZE fraction of
+/// candidate primes. This trades off with the increased cache/memory use of storing residues, which
+/// gives a practical limit of n = 2 * 3 * 5 * 7 for modern CPU + RAM combinations.
+///
+/// When striking a prime p, we need to find the multiples of p that occur exactly in spokes,
+/// otherwise we'd waste cycles missing spokes. Finding the next spoke multiple is done by
+/// exploiting the fact (Z / n Z)^x is a group. For our example where n = 2 * 3 with WHEEL = [1, 5],
+/// we know that the next multiples after p * 1 are p * 5, p * (6 + 1), p * (6 + 5), etc.
+/// Multiplying by any other residue would map us outside the group because the multiplicand is not
+/// a group element.
+///
+/// This iteration can be done efficiently by adding p multiplied by the gap _between_ spokes
+/// (stored in the SPOKE_GAP array) to the current multiple.
+///
+/// Iterating like this with a variable SPOKE_GAPS increment involves a multiplication and an
+/// addition to find each multiple. If we instead slice by spoke, then each iteration only requires
+/// adding p * WHEEL_SIZE, or equivalently, just the index p _within_ the spoke vector. Compilers
+/// can further optimize constant p-increment iteration with loop unrolling and SIMD instructions.
 struct Segment {
     spokes: [Spoke; Sieve::SPOKE_SIZE],
 }
